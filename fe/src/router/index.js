a172bdfc85ffcc8b01b81162b7c51c524c81e061
Thao Nguyen <nttn293205@gmail.com>
2026-01-26 18:17:30 +0700

    Update khung fe

---
+++ fe/src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// const routes = [
+//     {
+//         path: "/",
+//         name: "Home",
+//         component: Homepage,
+//         meta: { layout: "Client" },
+//     },
+// ]    
+
+export default router
